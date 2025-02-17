c20478a31216c29ed4858b220d50b3e1244b3ecd
kaviyadurairaj <kaviyadurairaj08@gmail.com>
2025-02-17 08:29:14 +0530

    functions

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,4 +1,29 @@
-function testFunction(){
-    console.log("this is a test function");
+function demoFunction(){
+    console.log("demo function")
 }
-testFunction()
+function demoFunction1(){
+    console.log("fuction without return type")
+}
+demoFunction()
+demoFunction1()
+function func(x){
+    console.log("This is function with parameter without return type",x)
+}
+func(2)
+function Func3(){
+    return "This is function with return type"
+}
+console.log(Func3());
+function testFunction4(x){
+    return x+1
+}
+console.log("This is function with parameter with return type",testFunction4(5));
+const add=function(a,b){
+    return a+b;
+}
+console.log(add(2,4));
+// Named function
+const mul=function multiply(a,b){
+    return a*b;
+}
+console.log(mul(5,5));
