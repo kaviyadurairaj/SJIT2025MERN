62ac69d9e454338e010b6a7075088ace79176254
kaviyadurairaj <kaviyadurairaj08@gmail.com>
2025-02-26 17:28:48 +0530

    changes3

--- sjitBackend/index.js
+++ sjitBackend/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const bcrypt = require("bcrypt");
+const bcrypt = require('bcryptjs');
 const cors = require("cors"); 
 
 dotenv.config();
